9bc6339f3441f9405884c12346acb9a0c65ed8a9
Tom Dalling <tom@tomdalling.com>
2022-06-04 12:45:09 +1000

    Initial commit

---
+++ lib/boot.rb
@@ -0,0 +1,3 @@
+$LOAD_PATH.unshift(__dir__) unless $LOAD_PATH.include?(__dir__)
+
+require "dd"
---
+++ spec/lib/dd_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe DD do
+  it "exists" do
+    expect(subject).not_to be_nil
+  end
+end
